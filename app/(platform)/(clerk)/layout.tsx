8f0577cfdf3dc0950a5538ba5aa3dfcabd591524
Allen Larson <127446190+allenlarson@users.noreply.github.com>
2023-11-17 16:40:09 -0500

    added organization

--- app/(platform)/(clerk)/layout.tsx
+++ app/(platform)/(clerk)/layout.tsx
@@ -1,14 +1,5 @@
-import { Footer } from '@/app/(marketing)/_components/footer';
-import { Navbar } from '@/app/(marketing)/_components/navbar';
-
 const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="h-full flex items-center justify-center">
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
-    </div>
-  );
+  return <div className="h-full flex items-center">{children}</div>;
 };
 
 export default ClerkLayout;
