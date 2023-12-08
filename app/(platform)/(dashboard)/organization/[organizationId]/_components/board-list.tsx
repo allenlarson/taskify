8693ad56eaaf663eaebeba6e8658736f65fe02b8
Allen Larson <127446190+allenlarson@users.noreply.github.com>
2023-12-07 23:05:52 -0500

    Started adding stripe 27m

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -7,6 +7,8 @@ import { Hint } from '@/components/hint';
 import { FormPopover } from '@/components/form/form-popover';
 import { db } from '@/lib/db';
 import { Skeleton } from '@/components/ui/skeleton';
+import { MAX_FREE_BOARDS } from '@/constants/boards';
+import { getAvailableCount } from '@/lib/org-limit';
 
 export const BoardList = async () => {
   const { orgId } = auth();
@@ -23,6 +25,8 @@ export const BoardList = async () => {
       createdAt: 'desc',
     },
   });
+
+  const availableCount = await getAvailableCount();
 
   return (
     <div className="space-y-4">
@@ -48,7 +52,9 @@ export const BoardList = async () => {
             className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">5 remaining</span>
+            <span className="text-xs">{`${
+              MAX_FREE_BOARDS - availableCount
+            } remaining`}</span>
             <Hint
               sideOffset={40}
               description={`Free Workspaces can have up to 5 open boards. For Unlimited boards upgrade this workspace`}
